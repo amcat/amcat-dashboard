c14c3b9a86aa032ab9970393de40b7976ddc7f4f
Martijn Bastiaan <martijn@hmbastiaan.nl>
2015-06-04 15:29:33 +0200

    Added cache fields and database indices

---
+++ dashboard/migrations/0006_auto_20150604_1317.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('dashboard', '0005_auto_20150604_1241'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='query',
+            name='cache_mimetype',
+            field=models.TextField(null=True),
+        ),
+        migrations.AddField(
+            model_name='query',
+            name='cache_uuid',
+            field=models.TextField(null=True),
+        ),
+        migrations.AlterField(
+            model_name='query',
+            name='amcat_query_id',
+            field=models.IntegerField(db_index=True),
+        ),
+    ]
